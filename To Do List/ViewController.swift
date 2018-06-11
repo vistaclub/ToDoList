b9f9eadf7125286bd1081fa7f3be3b5fbbf7d44c
Jason Wong <jawong71@gmail.com>
2018-06-11 10:31:39 -0700

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Jason Wong on 2018-06-11.
+//  Copyright © 2018 Jason Wong. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
